587a847e222f2456751e16345684926fd273eeea
cintiaafg <cintia.flores8361@alumnos.udg.mx>
2026-05-07 16:37:43 -0600

    Implementación de solicitudes HTTP

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+/*import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
@@ -68,6 +68,91 @@ class HomeScreen extends StatelessWidget {
             ),
           ),
         ],
+      ),
+    );
+  }
+}*/
+
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: PokemonScreen(),
+    );
+  }
+}
+
+class PokemonScreen extends StatefulWidget {
+  @override
+  State<PokemonScreen> createState() => _PokemonScreenState();
+}
+
+class _PokemonScreenState extends State<PokemonScreen> {
+  String nombre = "";
+  String imagen = "";
+  int altura = 0;
+  int peso = 0;
+
+  Future<void> obtenerPokemon() async {
+    final response = await http.get(
+      Uri.parse('https://pokeapi.co/api/v2/pokemon/pikachu'),
+    );
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+
+      setState(() {
+        nombre = data['name'];
+        imagen = data['sprites']['front_default'];
+        altura = data['height'];
+        peso = data['weight'];
+      });
+    } else {
+      throw Exception('Error al cargar Pokémon');
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    obtenerPokemon();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("PokéAPI App"),
+      ),
+      body: Center(
+        child: nombre.isEmpty
+            ? const CircularProgressIndicator()
+            : Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.network(imagen),
+                  Text(
+                    nombre.toUpperCase(),
+                    style: const TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text("Altura: $altura"),
+                  Text("Peso: $peso"),
+                ],
+              ),
       ),
     );
   }
